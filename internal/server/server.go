179380c1f7952bba3a17c62b5a08f6f827fb0711
Daniil Pershin <pershin.daniil.e@gmail.com>
2025-04-05 16:40:19 +0300

    feat: add build, deploy, and server
    - everything for deploy
    - new server with "hello, world" phrase
    - update linter

---
+++ internal/server/server.go
@@ -0,0 +1,72 @@
+// Package server provides a simple HTTP server.
+package server
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"golang.org/x/sync/errgroup"
+)
+
+const (
+	module          = "server"
+	shutdownTimeout = 3 * time.Second
+)
+
+// Server wraps an http.Server instance.
+type Server struct {
+	lg  *slog.Logger
+	srv *http.Server
+}
+
+// New returns a new Server with default routing.
+func New(lg *slog.Logger) *Server {
+	router := chi.NewRouter()
+	router.Get("/", func(w http.ResponseWriter, _ *http.Request) {
+		if _, err := w.Write([]byte("Hello, World!")); err != nil {
+			return
+		}
+	})
+
+	return &Server{
+		lg: lg.With("module", module),
+		srv: &http.Server{
+			Addr:              ":8080",
+			Handler:           router,
+			ReadHeaderTimeout: time.Second,
+		},
+	}
+}
+
+// Run starts the HTTP server and listens for the shutdown signal.
+// It returns when the server is gracefully shut down or an error occurs.
+func (s *Server) Run(ctx context.Context) error {
+	eg, ctx := errgroup.WithContext(ctx)
+
+	eg.Go(func() error {
+		<-ctx.Done()
+
+		gfCtx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
+		defer cancel()
+
+		s.lg.Info("graceful shutdown")
+		return s.srv.Shutdown(gfCtx) //nolint:contextcheck // graceful shutdown with new context
+	})
+
+	eg.Go(func() error {
+		s.lg.Info("listen and serve", "addr", s.srv.Addr)
+
+		if err := s.srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			return fmt.Errorf("run: %v", err)
+		}
+
+		return nil
+	})
+
+	return eg.Wait()
+}
